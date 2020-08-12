573d72a9b5df975c16725919c65eb41f69b6a85f
Eric Nerby <ericnerby@msn.com>
2020-08-12 10:52:07 -0400

    made number of circles responsive for consistent density

--- js/canvas.js
+++ js/canvas.js
@@ -11,7 +11,8 @@ let c = canvas.getContext('2d');
 
 // --- set variables
 let circleArray = [];
-const numberOfCircles = 500;
+let numberOfCircles = 500;
+const circleDensity = 1000;
 const colorArray = [
     [21, 176, 151],
     [92, 93, 141],
@@ -81,6 +82,8 @@ function animate() {
 
 //populate the circle array
 function init() {
+    const windowArea =  window.innerWidth * window.innerHeight;
+    numberOfCircles = windowArea / circleDensity;
     circleArray = [];
 
     for (let i = 0;i<numberOfCircles;i++) {
@@ -92,7 +95,6 @@ init();
 
 //start the animation loop
 animate();
-
 
 /* ----- Event Listeners ----- */
 window.addEventListener('mousemove', event => {
