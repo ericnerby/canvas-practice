9465db3a5c6935f8ff5776b1e575948093b51c66
Eric Nerby <ericnerby@msn.com>
2020-08-11 21:42:50 -0400

    update notes in Circle.js

--- js/Circle.js
+++ js/Circle.js
@@ -1,4 +1,15 @@
 class Circle {
+    /**
+     * constructs an object in the Circle class
+     * @param {number} x  - horizontal position
+     * @param {number} y  - vertical position
+     * @param {number} dx - horizontal velocity
+     * @param {number} dy - vertical velocity
+     * @param {number} radius - radius of circle
+     * @param {number} r  - red color value
+     * @param {number} g  - green color value
+     * @param {number} b  - blue color value
+     */
     constructor(x,y, dx, dy, radius, r,g,b) {
         this.x = x;
         this.y = y;
@@ -8,6 +19,9 @@ class Circle {
         this.color = `rgb(${r},${g},${b})`;
     }
 
+    /**
+     * draws the circle on the canvas
+     */
     draw = () => {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
@@ -15,6 +29,10 @@ class Circle {
         c.stroke();
     }
 
+    /**
+     * updates the location of the circle on the canvas
+     *   if the circle reaches the end of the canvas, it changes directions
+     */
     move = () => {
         this.draw();
         if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
